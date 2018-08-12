84b6ab8769783fea1829a4c5eb1a9599523dbc3e
fanghanzhang <handler_zhang@163.com>
2018-08-12 16:53:04 +0800

    添加vitamio

--- app/src/main/java/com/ironghui/datatree/activity/WebViewActivity.java
+++ app/src/main/java/com/ironghui/datatree/activity/WebViewActivity.java
@@ -3,15 +3,19 @@ package com.ironghui.datatree.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.ironghui.datatree.R;
 
 public class WebViewActivity extends AppCompatActivity {
 
     private WebView webView;
+    private ProgressBar mProgress;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,15 +25,46 @@ public class WebViewActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        mProgress = (ProgressBar) findViewById(R.id.mProgress);
         webView = findViewById(R.id.webview);
         WebSettings settings = webView.getSettings();
         settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-        webView.getSettings().setUseWideViewPort(true);
+      /*  webView.getSettings().setUseWideViewPort(true);
         webView.getSettings().setLoadWithOverviewMode(true);
          webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
          webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDomStorageEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+//        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebViewClient());
+        webView.loadUrl("https://www.baidu.com");*/
+        //支持js
+        webView.getSettings().setJavaScriptEnabled(true);
+        //支持缩放
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        //接口回调
+        webView.setWebChromeClient(new WebViewClient());
+        //加载网页
         webView.loadUrl("https://www.baidu.com");
+        new android.webkit.WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl("https://www.baidu.com");
+                return true;
+            }
+        };
+
+    }
+
+    public class WebViewClient extends WebChromeClient {
+        //进度变化监听
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            if (newProgress == 100) {
+                mProgress.setVisibility(View.GONE);
+            }
+            super.onProgressChanged(view, newProgress);
+        }
     }
 }
